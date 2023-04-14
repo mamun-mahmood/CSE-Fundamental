7c47b244a399bbc023bb76c949af67feb48d43c3
mamun-mahmood <mamamun1999@gmail.com>
2023-04-14 15:42:28 +0600

    reverse array c

--- CSE Fundamental/C/practice/reverse_array.c
+++ CSE Fundamental/C/practice/reverse_array.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int arr[5];
+    int arrSize, arr[5];
+    scanf("%d", &arrSize);
     for (int i = 0; i < 5; i++)
     {
         scanf("%d", &arr[i]);
     }
-    for (int i = 4; i >= 0; i--)
+    for (int i = arrSize - 1; i >= 0; i--)
     {
         printf("%d\n", arr[i]);
     }
