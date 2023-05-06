083fb35f0e40680a4bac3b3cf38989fb669fb007
mamun-mahmood <mamamun1999@gmail.com>
2023-05-07 02:12:27 +0600

    strcomp used

--- Intoduction to C Programming/string_compare.c
+++ Intoduction to C Programming/string_compare.c
@@ -5,36 +5,51 @@ int main()
     char a[100], b[100];
     scanf("%s %s", &a, &b);
     int i = 0;
-    while (1)
+    // custom way
+    // while (1)
+    // {
+    //     if (a[i] == '\0' && b[i] == '\0')
+    //     {
+    //         printf("Same");
+    //         break;
+    //     }
+    //     else if (a[i] == '\0')
+    //     {
+    //         printf("A is smaller");
+    //         break;
+    //     }
+    //     else if (b[i] == '\0')
+    //     {
+    //         printf("B is smaller");
+    //         break;
+    //     }
+    //     if (a[i] == b[i])
+    //     {
+    //         i++;
+    //     }
+    //     else if (a[i] < b[i])
+    //     {
+    //         printf("A is smaller");
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         printf("B is smaller");
+    //         break;
+    //     }
+    // }
+    // built in function
+    int result = strcmp(a, b);
+    if (result > 0)
     {
-        if (a[i] == '\0' && b[i] == '\0')
-        {
-            printf("Same");
-            break;
-        }
-        else if (a[i]=='\0')
-        {
-            printf("A is smaller");
-            break;
-        }
-        else if (b[i]=='\0')
-        {
-            printf("B is smaller");
-            break;
-        }
-        if (a[i] == b[i])
-        {
-            i++;
-        }
-        else if (a[i] < b[i])
-        {
-            printf("A is smaller");
-            break;
-        }
-        else
-        {
-            printf("B is smaller");
-            break;
-        }
+        printf("B is smaller");
+    }
+    else if (result < 0)
+    {
+        printf("A is smaller");
+    }
+    else
+    {
+        printf("Same");
     }
 }
