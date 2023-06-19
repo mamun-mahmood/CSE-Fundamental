053fabef47cb6dad62dd2b59b1e0c67723a0962c
mamun-mahmood <mamamun1999@gmail.com>
2023-06-19 11:57:09 +0600

    Shape_T

--- Problem Solving/W_Shape_3.c
+++ Problem Solving/W_Shape_3.c
@@ -5,18 +5,28 @@ int main()
     scanf("%d", &n);
     int num_of_str = 1;
     int num_of_space = n - 1;
+    n *= 2;
+    int num_of_line = n / 2;
     for (int i = 1; i <= n; i++)
     {
         for (int space = num_of_space; space > 0; space--)
         {
             printf(" ");
         }
-        num_of_space--;
         for (int str = 0; str < num_of_str; str++)
         {
             printf("*");
         }
-        num_of_str += 2;
+        if (i > num_of_line+1)
+        {
+            num_of_space++;
+            num_of_str -= 2;
+        }
+        else if(i<num_of_line)
+        {
+            num_of_space--;
+            num_of_str += 2;
+        }
         printf("\n");
     }
 }
