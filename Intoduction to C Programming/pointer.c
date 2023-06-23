f0c5798235766acb2005104d34c6ac490e42cdd9
mamun-mahmood <mamamun1999@gmail.com>
2023-06-23 11:47:49 +0600

    poninter

--- Intoduction to C Programming/pointer.c
+++ Intoduction to C Programming/pointer.c
@@ -5,7 +5,7 @@ int main()
     printf("%p\n", &x);
     // reference operator
     int *ptr_of_x = &x;
-    printf("%p\n", ptr_of_x);
+    printf("%d\n", sizeof(ptr_of_x));
     // dereference operator
     printf("%d\n", *ptr_of_x);
     // overwriting the value of x
