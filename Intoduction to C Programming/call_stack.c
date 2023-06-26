d43f7bc3adb1df9b2d4e55bc851ff256c295ee6c
mamun-mahmood <mamamun1999@gmail.com>
2023-06-26 11:28:33 +0600

    call stack

--- Intoduction to C Programming/call_stack.c
+++ Intoduction to C Programming/call_stack.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 void world_print(void)
 {
-    printf("World sttack 3\n");
+    printf("World stack 3\n");
 }
 void hello_print(void)
 {
-    printf("Hello stack 2\n");
+    printf("Hello start stack 2\n");
+    world_print();
+    printf("Hello end stack 4\n");
 }
 
 int main()
 {
     printf("Start stack 1 \n");
     hello_print();
-    world_print();
-    printf("End stack 4 ");
+    printf("End stack 5 ");
     return 0;
 }
